3a5b0adee9ace6d31110c0aaa042e4823f126cbd
rubasankar <rubasankar2002@gmail.com>
2024-08-10 22:33:50 +0530

    v0.2 lint fix

--- StickyNotes/api/views.py
+++ StickyNotes/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from StickyNotes.models import StickyNote
 from StickyNotes.api.serializers import NoteSerializer
 
+
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
@@ -17,12 +18,14 @@ class NoteListCreate(generics.ListCreateAPIView):
             print("errors")
             print(serializer.errors)
 
+
 class NoteDelete(generics.DestroyAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return StickyNote.objects.filter(author=self.request.user)
+
 
 class NoteUpdate(generics.RetrieveUpdateAPIView):
     serializer_class = NoteSerializer
